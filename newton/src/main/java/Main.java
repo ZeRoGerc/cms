4faaa019f08c5b3683156f9635b29c523dd6eaf7
Uladzislau Sazanovich <zerogerc@gmail.com>
2016-11-05 22:44:53 +0300

    project set up

---
+++ newton/src/main/java/Main.java
@@ -0,0 +1,13 @@
+
+import static java.lang.System.out;
+import org.joda.time.LocalTime;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        LocalTime currentTime = new LocalTime();
+        out.println("The current local time is: " + currentTime);
+        out.println("Hello World!");
+    }
+}

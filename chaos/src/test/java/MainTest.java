ebbdccf77848dadd7af8f80b87c22dfa949665d6
Uladzislau Sazanovich <zerogerc@gmail.com>
2016-11-30 16:16:25 +0300

    chaos initial

---
+++ chaos/src/test/java/MainTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MainTest {
+
+    @Test
+    public void testPlus() throws Exception {
+        assertEquals(2 + 2, 4);
+    }
+}
